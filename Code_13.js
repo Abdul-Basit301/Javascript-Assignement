45f4504dfba1d71f84c96bccab65daa2220c2e5c
Muhammad Basit Kamal <54136833+Abdul-Basit301@users.noreply.github.com>
2022-10-28 20:29:21 +0500

    Create Code_13.js

---
+++ Code_13.js
@@ -0,0 +1,5 @@
+let transport=["Mortorcycle","Car","Bus","Airplane"];
+console.log(`I love ${transport[0]}.`);
+console.log(`${transport[1]}s are passion of mine.`);
+console.log(`Headphones with your favorite playlist and travelling in ${transport[2]} is the moment to live.`);
+console.log(`Flying in ${transport[3]} and traveling is my hobby.`);
