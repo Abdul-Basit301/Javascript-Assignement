b98471dedb388f7c0739a838c0afc9d6d8197a09
Muhammad Basit Kamal <54136833+Abdul-Basit301@users.noreply.github.com>
2022-10-28 14:52:25 +0500

    Create Code_12.js

---
+++ Code_12.js
@@ -0,0 +1,6 @@
+const names=["Salik","Rehman","Ahmed","Zain"];
+console.log(names);
+console.log(`Hello ${names[0]}, How are you?`);
+console.log(`Hello ${names[1]}, How are you?`);
+console.log(`Hello ${names[2]}, How are you?`);
+console.log(`Hello ${names[3]}, How are you?`);
