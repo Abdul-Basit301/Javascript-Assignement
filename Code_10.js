61f6062d42830f996c7a6e8e614b972dac44bcfe
Muhammad Basit Kamal <54136833+Abdul-Basit301@users.noreply.github.com>
2022-10-28 12:44:22 +0500

    Update Code_10.js

--- Code_10.js
+++ Code_10.js
@@ -5,4 +5,9 @@ let famous_person="Albert Einstein";
 let message="A person who never made a mistake never tried anything new."
 console.log(`${famous_person} once said,"${message}"`);
 
-
+//Muhammad Basit Kamal date=28-10-2022
+//adding, subtracting, multiplication and division to return 8 as an output.
+console.log(5+3);
+console.log(9-1);
+console.log(4*2);
+console.log(16/2);
