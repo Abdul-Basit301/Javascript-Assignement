77db1ab28055188daad40eefa7f51a355dbee7ca
Muhammad Basit Kamal <54136833+Abdul-Basit301@users.noreply.github.com>
2022-10-28 12:42:41 +0500

    Update Code_10.js

--- Code_10.js
+++ Code_10.js
@@ -1,1 +1,8 @@
+//Muhammad Basit Kamal date=28-10-2022
+
+//storing person name in variable and its famous quote in another variable and print it.
+let famous_person="Albert Einstein";
+let message="A person who never made a mistake never tried anything new."
+console.log(`${famous_person} once said,"${message}"`);
+
 
